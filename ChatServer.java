90fb1ad99dac46424926e7a598f7e808592ad195
g1345468 <bonds-3164@Ocha-2.local>
2015-07-29 20:10:05 +0900

    refs #1 ChatServerクラスを用意した．
    Singletonパターンを用いてChatServerクラスを作成した．

---
+++ ChatServer.java
@@ -0,0 +1,11 @@
+public class ChatServer{
+
+    private static ChatServer chatServer = new ChatServer();
+
+    private ChatServer() {
+    }
+    
+    public static void main(String[] args){	
+    }
+
+}
